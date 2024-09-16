02545cfb6219878740cc65558ffa392fa33ef748
William Crispim <williamcrispim@live.com>
2024-09-16 20:18:19 -0300

    add command line reader

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
+const readline = require('readline')
 const parser = require('./gramatica');  // Importa o parser gerado pelo PEG.js
 let variaveis = {};  // Armazena os valores das variáveis
+
+// Configura o readline para aceitar entradas do terminal
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Digite uma expressão ou pressione ENTER para processar: '
+});
+
+let entradas = [];
+
+rl.prompt();
+
+rl.on('line', (line) => {
+  if (line.trim() === '') {
+    // Quando uma linha vazia é inserida, processa todas as entradas
+    console.log('Processando as entradas...');
+    processarEntrada(entradas.join('\n'));
+    const variaveisKeys = Object.keys(variaveis);
+    if (variaveisKeys.length > 0) {
+      variaveisKeys.forEach(key => console.log(`A variável ${key} tem um valor final de: ${variaveis[key]}.`));
+    }
+    rl.close();  // Fecha o readline após o processamento
+  } else {
+    // Adiciona a entrada atual ao array de entradas
+    entradas.push(line);
+    rl.prompt();  // Solicita nova entrada
+  }
+}).on('close', () => {
+  console.log('Fim do programa.');
+  process.exit(0);
+});
 
 // Avalia uma expressão com base em sua estrutura
 function avaliarExpressao(expr) {
@@ -37,8 +69,12 @@ function processarEntrada(entrada) {
   const resultadoAux = parser.parse(entrada.trim());
   const resultado = [];
   resultadoAux.forEach(element => {
-    if (Array.isArray(element)) resultado.push(...element.filter(element => element.hasOwnProperty("type") && element.hasOwnProperty("variable") && element.hasOwnProperty("value")))
-    else return element
+    if (Array.isArray(element)) resultado.push(
+      ...element.filter(element => {
+        return element.hasOwnProperty("type") && element.hasOwnProperty("value") && (element.hasOwnProperty("variable") || element.hasOwnProperty("tail"))
+      })
+    )
+      else return element
   })
   resultado.forEach(statement => {
     if (statement.type === 'assignment') {
@@ -46,17 +82,7 @@ function processarEntrada(entrada) {
       variaveis[statement.variable] = avaliarExpressao(statement.value);
     } else {
       // Calcula e imprime os resultados de expressões livres
-      console.log(`Resultado: ${avaliarExpressao(statement)}`);
+      console.log(`O resultado da expressão livre digitada é: ${avaliarExpressao(statement)}.`);
     }
   });
 }
-
-// Exemplo de uso da função de processamento
-const entrada = `
-  a := 5.2 + 3
-  b := 5 + 3
-  c := a + b
-`;
-
-processarEntrada(entrada);
-console.log(variaveis);  // Exibe os valores finais das variáveis
